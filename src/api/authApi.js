774d6d98d5f03829a64fefd6fd16536e7f3b2e4c
Phan Vy <phanvy@MacBook-Air-cua-Phan.local>
2025-10-07 09:17:53 +0700

    feat(hacker): ban da bi hack

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,8 +2,9 @@
 import axiosInstance from "./axiosInstance";
 
 const authApi = {
+    //Hello cac ban, toi la hacker, cac ban da bi hack
     signup: (data) => axiosInstance.post("/api/v1/auth/signup", data),
-    signin: (data) => axiosInstance.post("/api/v1/auth/signin", data), // ✅ thêm dòng này
+    signin: (data) => axiosInstance.post("/api/v1/auth/signin", data), // thêm dòng này
     verifyOtp: (data) => axiosInstance.post("/api/v1/auth/verify-otp", data),
 };
 
